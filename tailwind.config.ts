7ae17ec5bcc8e6900c615bc97c2f23589f4dd091
surya3652 <gajulasurya7@gmail.com>
2026-01-25 13:58:13 +0530

    Tailwind setup

---
+++ tailwind.config.ts
@@ -0,0 +1,42 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "base-color": "#c2001c",
+        "dark-gray": "#232323",
+        "light-medium-gray": "#e9e9e9",
+        "extra-medium-gray": "#999",
+      },
+      fontFamily: {
+        primary: ["var(--font-quicksand)", "sans-serif"],
+      },
+      fontSize: {
+        "335": "20.938rem",
+        "150": "9.375rem",
+        "70": "4.375rem",
+        "50": "3.125rem",
+      },
+      letterSpacing: {
+        "minus-10px": "-10px",
+        "minus-5px": "-5px",
+        "minus-3px": "-3px",
+      },
+      spacing: {
+        section: "90px",
+        "section-tablet": "75px",
+        "section-mobile": "50px",
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
